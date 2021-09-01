929220233c8623cc3482d4400d85d480d4f461e5
Poskvansk <luisfilipesrct@gmail.com>
2021-09-01 14:29:44 -0300

    pronto

--- src/State.cpp
+++ src/State.cpp
@@ -17,10 +17,8 @@ void State::Update(float dt) {
 }
 
 void State::LoadAssets() {
-    //std::cout << &this->music << std::endl;
-    //std::cout << &this->bg << std::endl;
-    //this->music = new Music("stageState.ogg");
-    //this->bg = new Sprite();
+    // bg = new Sprite("assets/img/ocean.jpg");
+    // music = new Music("assets/audio/stageState.ogg");
 }
 
 void State::Render() {
